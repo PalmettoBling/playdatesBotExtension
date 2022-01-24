a61ea39c83c90f17808345333958131e79bb4384
Merrick <palmettobling@gmail.com>
2022-01-23 22:35:40 -0500

    Added quote submission functionality.

--- popup/index.js
+++ popup/index.js
@@ -2,12 +2,9 @@ const randomQuoteUrl = "https://www.xboxplaydates.us/playdatesQuotes/randomquote
 let response = fetch(randomQuoteUrl)
     .then(response => response.json())
     .then(data => {
-        console.log("This is a test");
-        console.log(response);
-        console.log(data);
         document.getElementById('quotenumber').innerHTML = "#" + data.id;
         document.getElementById('quotetext').innerHTML = data.quote + " - " + data.attribution;
         document.getElementById('quotedata').innerHTML = data.game + " - " + data.dateOfQuote;
     }).catch(error => {
-        alert("HTTP-Error: " + response.status);
+        alert("HTTP-Error: " + error);
     });
---
+++ quotes/addQuote.js
@@ -0,0 +1,36 @@
+/*let formsubmission = document.getElementById("quotebutton");
+if (formsubmission) {
+    formsubmission.onclick = function() {
+        document.getElementById('quoteform').reset();
+        document.getElementById('quotearea').innerHTML +=
+            "<h3>Quote has been submitted for review.</h3>";
+    }
+}
+*/
+onload = function() {
+    document.querySelector('#quoteform').onsubmit = function(e) {
+        e.preventDefault();
+        var quotetext = document.forms["quoteform"]["quotetext"];
+        var quoteattribution = document.forms["quoteform"]["quoteattribution"]
+        var quotegame = document.forms["quoteform"]["quotegame"];
+        var quotedate = document.forms["quoteform"]["quotedate"];
+    
+        let addQuoteUrl = "https://www.xboxplaydates.us/api/addquote";
+        let response = fetch(addQuoteUrl, {
+            method: "POST", 
+            body: JSON.stringify({
+                "quotetext": quotetext.value,
+                "quoteattribution": quoteattribution.value,
+                "quotegame": quotegame.value,
+                "quotedate": quotedate.value
+            }),
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            }
+        }).then(response => {
+            console.log("Response: " + response);
+            this.reset();
+            this.innerHTML += "<h3>Quote has been submitted for review.</h3>";
+        });    
+    }
+}
